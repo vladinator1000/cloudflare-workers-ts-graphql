89e4073e54382176b43eec73a209aa9a03e18e57
Vlady Veselinov <vladkodmc@gmail.com>
2022-01-21 00:47:39 +0000

    change migration file to trigger action

--- src/prisma/migrations/20220120000808_init/migration.sql
+++ src/prisma/migrations/20220120000808_init/migration.sql
@@ -1,7 +1,7 @@
 -- CreateEnum
 CREATE TYPE "Level" AS ENUM ('Info', 'Warn', 'Error');
 
--- CreateTable
+-- CreateTabl
 CREATE TABLE "Log" (
     "id" SERIAL NOT NULL,
     "level" "Level" NOT NULL,
