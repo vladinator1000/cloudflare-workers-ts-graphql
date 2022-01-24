8eb6fb904fed1613baf3a3a28e50fc95d1b6046c
Vlady Veselinov <vladkodmc@gmail.com>
2022-01-24 02:59:39 +0000

    fix example log resolver

--- src/graphql/log/log.resolvers.ts
+++ src/graphql/log/log.resolvers.ts
@@ -4,7 +4,7 @@ import { prismaLogToGql } from './log.mappers'
 export const logResolvers: Resolvers = {
   Query: {
     async logs(_, __, { prisma }) {
-      const logs = await prisma.log.findMany().then()
+      const logs = await prisma.log.findMany()
       return logs.map(prismaLogToGql)
     },
   },
